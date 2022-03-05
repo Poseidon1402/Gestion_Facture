3d2b0ef28adb145f1d4dc28b7202e9223a011191
RAJOELISON Aina Tiavina <rajoelisonainatiavina@gmail.com>
2022-03-05 19:18:44 +0300

    stylizing create client page

--- src/Form/ClientType.php
+++ src/Form/ClientType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 use Vich\UploaderBundle\Form\Type\VichImageType;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
@@ -16,20 +17,27 @@ class ClientType extends AbstractType
     {
         $builder
             ->add('Numcli', TextType::class, [
+                'label' => 'Numéro du Client',
                 'attr' => [
                     'readOnly' => true,
                 ]
             ])
             ->add('Nom', TextType::class, [
-                'label' => 'Noms: ',
+                'label' => 'Nom',
                 'constraints' => [
                     new NotBlank([
                         'message' => 'This field cannot be empty.'
+                    ]),
+                    new Length([
+                        'min' => 3,
+                        'minMessage' => "Le nom doit contenir au moins { limit } caractères.",
+                        'max' => 150,
+                        'maxMessage' => "Le nom ne peut contenir que { limit } caractères"
                     ])
                 ]
             ])
             ->add('imageFile', VichImageType::class, [
-                'label' => 'Upload Image(PNG or JPG)',
+                'label' => 'Upload Image (PNG or JPG)',
                 'required' => false,
                 'allow_delete' => true,
                 'download_uri' => false,
