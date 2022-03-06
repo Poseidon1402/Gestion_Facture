fb41b940c8aa054c654f4b5ead7ab8c82e3d721a
AntonioHery <antonioheryramamonjisoa@gmail.com>
2022-03-06 09:41:23 -0800

    add sideBar and customize commande

--- migrations/Version20220303172925.php
+++ migrations/Version20220303172925.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220302122112 extends AbstractMigration
+final class Version20220303172925 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,16 +20,17 @@ final class Version20220302122112 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE commande (id INT AUTO_INCREMENT NOT NULL, clients_id VARCHAR(16) NOT NULL, produits_id VARCHAR(16) NOT NULL, qte INT NOT NULL, date_commande DATE NOT NULL, INDEX IDX_6EEAA67DAB014612 (clients_id), INDEX IDX_6EEAA67DCD11A2CF (produits_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE commande ADD CONSTRAINT FK_6EEAA67DAB014612 FOREIGN KEY (clients_id) REFERENCES client (numcli)');
-        $this->addSql('ALTER TABLE commande ADD CONSTRAINT FK_6EEAA67DCD11A2CF FOREIGN KEY (produits_id) REFERENCES produit (numPro)');
+        $this->addSql('ALTER TABLE client CHANGE Numcli numcli VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE commande CHANGE clients_id clients_id VARCHAR(255) NOT NULL, CHANGE produits_id produits_id VARCHAR(10) NOT NULL');
+        $this->addSql('ALTER TABLE commande ADD CONSTRAINT FK_6EEAA67DCD11A2CF FOREIGN KEY (produits_id) REFERENCES produit (num_pro)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE commande');
-        $this->addSql('ALTER TABLE client CHANGE numcli Numcli VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE nom Nom VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE image_name image_name VARCHAR(255) DEFAULT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE client CHANGE numcli Numcli VARCHAR(10) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE nom Nom VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE image_name image_name VARCHAR(255) DEFAULT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE commande DROP FOREIGN KEY FK_6EEAA67DCD11A2CF');
+        $this->addSql('ALTER TABLE commande CHANGE clients_id clients_id VARCHAR(16) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE produits_id produits_id VARCHAR(16) NOT NULL COLLATE `utf8mb4_unicode_ci`');
         $this->addSql('ALTER TABLE messenger_messages CHANGE body body LONGTEXT NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE headers headers LONGTEXT NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE queue_name queue_name VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`');
         $this->addSql('ALTER TABLE produit CHANGE num_pro num_pro VARCHAR(10) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE design design VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`');
     }
